10b652e162c6c6a40feb0ce7a58322ce489f6efa
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-11-23 18:39:26 -0300

    fix: product id strategy

--- src/main/java/com/vinibortoletto/simpleshop/models/ProductModel.java
+++ src/main/java/com/vinibortoletto/simpleshop/models/ProductModel.java
@@ -15,7 +15,7 @@ public class ProductModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
     private String name;
