20857946493884cc12a95231bd295984a97b2fb6
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-11-27 12:12:24 -0300

    feat: find by id and save product

--- src/main/java/com/vinibortoletto/simpleshop/controllers/ProductController.java
+++ src/main/java/com/vinibortoletto/simpleshop/controllers/ProductController.java
@@ -1,15 +1,17 @@
 package com.vinibortoletto.simpleshop.controllers;
 
+import com.vinibortoletto.simpleshop.dtos.ProductDto;
 import com.vinibortoletto.simpleshop.models.ProductModel;
 import com.vinibortoletto.simpleshop.services.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -21,5 +23,21 @@ public class ProductController {
     public ResponseEntity<List<ProductModel>> findAll() {
         List<ProductModel> productList = service.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(productList);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ProductModel> findById(@PathVariable String id) {
+        System.out.println(id);
+        ProductModel product = service.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(product);
+    }
+
+    @PostMapping()
+    public ResponseEntity<ProductModel> save(@RequestBody @Valid ProductDto dto) {
+        ProductModel product = new ProductModel();
+        BeanUtils.copyProperties(dto, product);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(service.save(product));
     }
 }
--- src/main/java/com/vinibortoletto/simpleshop/services/ProductService.java
+++ src/main/java/com/vinibortoletto/simpleshop/services/ProductService.java
@@ -1,11 +1,15 @@
 package com.vinibortoletto.simpleshop.services;
 
+import com.vinibortoletto.simpleshop.dtos.ProductDto;
+import com.vinibortoletto.simpleshop.exceptions.NotFoundException;
 import com.vinibortoletto.simpleshop.models.ProductModel;
 import com.vinibortoletto.simpleshop.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ProductService {
@@ -14,5 +18,14 @@ public class ProductService {
 
     public List<ProductModel> findAll() {
         return repository.findAll();
+    }
+
+    public ProductModel findById(String id) {
+        Optional<ProductModel> product = repository.findById(id);
+        return product.orElseThrow(() -> new NotFoundException("Product not found"));
+    }
+
+    public ProductModel save(ProductModel newProduct) {
+        return repository.save(newProduct);
     }
 }
