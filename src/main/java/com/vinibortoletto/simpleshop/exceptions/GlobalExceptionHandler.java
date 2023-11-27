1e643b0bfc48bd719706367dc32a53d12ff053c7
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-11-27 13:37:17 -0300

    refactor: lombok annotations

--- src/main/java/com/vinibortoletto/simpleshop/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/vinibortoletto/simpleshop/exceptions/GlobalExceptionHandler.java
@@ -3,12 +3,12 @@ package com.vinibortoletto.simpleshop.exceptions;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<CustomException> notFound(NotFoundException exception, HttpServletRequest request) {
