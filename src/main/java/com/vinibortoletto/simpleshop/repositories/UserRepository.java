ad1aa3052876f4333b5027a3d3b68499bfd25d88
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-11-27 19:02:32 -0300

    chore: add find by email to user repository

--- src/main/java/com/vinibortoletto/simpleshop/repositories/UserRepository.java
+++ src/main/java/com/vinibortoletto/simpleshop/repositories/UserRepository.java
@@ -4,6 +4,9 @@ import com.vinibortoletto.simpleshop.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
