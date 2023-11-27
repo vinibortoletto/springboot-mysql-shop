14c5781292fbdab50b873e91f1a1c4997d6bf9ef
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-11-27 13:36:53 -0300

    refactor: product id

--- src/main/java/com/vinibortoletto/simpleshop/models/ProductModel.java
+++ src/main/java/com/vinibortoletto/simpleshop/models/ProductModel.java
@@ -1,16 +1,15 @@
 package com.vinibortoletto.simpleshop.models;
 
 import jakarta.persistence.*;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode
@@ -20,24 +19,11 @@ public class ProductModel implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String name;
     private BigDecimal price;
     private Integer stock;
     private String image;
     private String description;
-
-    public ProductModel() {
-        this.id = String.valueOf(UUID.randomUUID());
-    }
-
-    public ProductModel(String name, BigDecimal price, Integer stock, String image, String description) {
-        this.id = String.valueOf(UUID.randomUUID());
-        this.name = name;
-        this.price = price;
-        this.stock = stock;
-        this.image = image;
-        this.description = description;
-    }
 }
