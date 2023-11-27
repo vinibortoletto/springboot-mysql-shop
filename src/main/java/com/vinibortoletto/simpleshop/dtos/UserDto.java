23f4e7e225bb6b0801ef90f38c925bc362db9689
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-11-27 18:58:49 -0300

    fix: user dto

--- src/main/java/com/vinibortoletto/simpleshop/dtos/UserDto.java
+++ src/main/java/com/vinibortoletto/simpleshop/dtos/UserDto.java
@@ -5,11 +5,13 @@ import com.vinibortoletto.simpleshop.models.Address;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.List;
+
 public record UserDto(
         @NotBlank String name,
         @NotBlank String email,
         @NotBlank String phone,
-        @NotNull Address address,
+        @NotNull List<Address> addresses,
         @NotBlank String password,
         @NotNull Role role
 ) {
