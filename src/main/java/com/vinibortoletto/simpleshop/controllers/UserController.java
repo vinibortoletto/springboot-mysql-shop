7cafeb1cce1d1d21781d60ad5b50531f14716bb3
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-11-27 18:52:31 -0300

    feat: find user by id

--- src/main/java/com/vinibortoletto/simpleshop/controllers/UserController.java
+++ src/main/java/com/vinibortoletto/simpleshop/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.vinibortoletto.simpleshop.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,9 @@ public class UserController {
         return ResponseEntity.ok().body(userList);
     }
 
-
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable String id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
 }
--- src/main/java/com/vinibortoletto/simpleshop/services/UserService.java
+++ src/main/java/com/vinibortoletto/simpleshop/services/UserService.java
@@ -1,11 +1,13 @@
 package com.vinibortoletto.simpleshop.services;
 
+import com.vinibortoletto.simpleshop.exceptions.NotFoundException;
 import com.vinibortoletto.simpleshop.models.User;
 import com.vinibortoletto.simpleshop.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +16,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = repository.findById(id);
+        return user.orElseThrow(() -> new NotFoundException("User not found"));
     }
 }
