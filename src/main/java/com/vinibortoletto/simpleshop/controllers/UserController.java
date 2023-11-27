d19ddba0d86237a9a8b88da9ea75ddcfd1c467b0
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-11-27 19:02:52 -0300

    feat: save user

--- src/main/java/com/vinibortoletto/simpleshop/controllers/UserController.java
+++ src/main/java/com/vinibortoletto/simpleshop/controllers/UserController.java
@@ -1,13 +1,13 @@
 package com.vinibortoletto.simpleshop.controllers;
 
+import com.vinibortoletto.simpleshop.dtos.UserDto;
 import com.vinibortoletto.simpleshop.models.User;
 import com.vinibortoletto.simpleshop.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +27,10 @@ public class UserController {
     public ResponseEntity<User> findById(@PathVariable String id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody @Valid UserDto dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(dto));
     }
 }
--- src/main/java/com/vinibortoletto/simpleshop/services/UserService.java
+++ src/main/java/com/vinibortoletto/simpleshop/services/UserService.java
@@ -1,8 +1,11 @@
 package com.vinibortoletto.simpleshop.services;
 
+import com.vinibortoletto.simpleshop.dtos.UserDto;
+import com.vinibortoletto.simpleshop.exceptions.ConflictException;
 import com.vinibortoletto.simpleshop.exceptions.NotFoundException;
 import com.vinibortoletto.simpleshop.models.User;
 import com.vinibortoletto.simpleshop.repositories.UserRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +24,17 @@ public class UserService {
     public User findById(String id) {
         Optional<User> user = repository.findById(id);
         return user.orElseThrow(() -> new NotFoundException("User not found"));
+    }
+
+    public User save(UserDto dto) {
+        Optional<User> user = repository.findByEmail(dto.email());
+
+        if (user.isPresent()) {
+            throw new ConflictException("Email already exists in database");
+        }
+
+        User newUser = new User();
+        BeanUtils.copyProperties(dto, newUser);
+        return repository.save(newUser);
     }
 }
