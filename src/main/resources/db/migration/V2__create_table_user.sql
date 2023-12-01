2f3cbb19af03ad97e7bb7ce0f5454ed82da2fe6f
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-01 17:07:47 -0300

    chore: update sql scripts

--- src/main/resources/db/migration/V2__create_table_user.sql
+++ src/main/resources/db/migration/V2__create_table_user.sql
@@ -2,7 +2,7 @@ CREATE TABLE tb_user (
     id VARCHAR(36) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    phone VARCHAR(20) NOT NULL,
+    phone VARCHAR(20),
     password VARCHAR(255) NOT NULL,
     role ENUM("ADMIN", "SELLER", "CUSTOMER") NOT NULL
 );
