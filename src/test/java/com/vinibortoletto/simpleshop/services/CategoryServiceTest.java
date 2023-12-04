5b80de409ff2314bde1ef57b9a9771e5a6c25e82
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-04 17:18:23 -0300

    test: find category by id

---
+++ src/test/java/com/vinibortoletto/simpleshop/services/CategoryServiceTest.java
@@ -0,0 +1,54 @@
+package com.vinibortoletto.simpleshop.services;
+
+import com.vinibortoletto.simpleshop.exceptions.NotFoundException;
+import com.vinibortoletto.simpleshop.fakers.CategoryFaker;
+import com.vinibortoletto.simpleshop.models.Category;
+import com.vinibortoletto.simpleshop.repositories.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+class CategoryServiceTest {
+    private final CategoryFaker categoryFaker = new CategoryFaker();
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    @InjectMocks
+    private CategoryService categoryService;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    @DisplayName("findById - should throw exception if category is not found")
+    void findByIdCase1() {
+        String id = String.valueOf(UUID.randomUUID());
+        when(categoryRepository.findById(id)).thenReturn(Optional.empty());
+        assertThrows(NotFoundException.class, () -> categoryService.findById(id));
+    }
+
+    @Test
+    @DisplayName("findById - should find category by id")
+    void findByIdCase2() {
+        Category category = categoryFaker.createFakeCategory();
+        when(categoryRepository.findById(category.getId())).thenReturn(Optional.of(category));
+        Category foundCategory = categoryService.findById(category.getId());
+        assertEquals(category, foundCategory);
+        verify(categoryRepository, times(1)).findById(category.getId());
+    }
+}
