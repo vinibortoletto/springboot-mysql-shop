fc73e9d4e82188b4a3210878d2dff675fec645e5
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-04 17:51:40 -0300

    feat: find order by id

--- src/main/java/com/vinibortoletto/simpleshop/controllers/OrderController.java
+++ src/main/java/com/vinibortoletto/simpleshop/controllers/OrderController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,11 @@ public class OrderController {
     @GetMapping()
     public ResponseEntity<List<Order>> findAll() {
         return ResponseEntity.ok().body(orderService.findAll());
+    }
+
+    @Operation(summary = "Finds an order by id")
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Order> findById(@PathVariable String id) {
+        return ResponseEntity.ok().body(orderService.findById(id));
     }
 }
--- src/main/java/com/vinibortoletto/simpleshop/services/OrderService.java
+++ src/main/java/com/vinibortoletto/simpleshop/services/OrderService.java
@@ -1,11 +1,13 @@
 package com.vinibortoletto.simpleshop.services;
 
+import com.vinibortoletto.simpleshop.exceptions.NotFoundException;
 import com.vinibortoletto.simpleshop.models.Order;
 import com.vinibortoletto.simpleshop.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderService {
@@ -15,5 +17,10 @@ public class OrderService {
 
     public List<Order> findAll() {
         return orderRepository.findAll();
+    }
+
+    public Order findById(String id) {
+        Optional<Order> order = orderRepository.findById(id);
+        return order.orElseThrow(() -> new NotFoundException("Order not found"));
     }
 }
