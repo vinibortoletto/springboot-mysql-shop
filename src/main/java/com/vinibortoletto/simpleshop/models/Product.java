2e0ccefb47582b21f6a6ae81e234e366842b63e5
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-04 14:12:08 -0300

    feat: join categories and products

--- src/main/java/com/vinibortoletto/simpleshop/models/Product.java
+++ src/main/java/com/vinibortoletto/simpleshop/models/Product.java
@@ -6,6 +6,8 @@ import lombok.*;
 import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -18,11 +20,18 @@ public class Product implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String name;
     private BigDecimal price;
     private Integer stock;
     private String image;
     private String description;
+
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private List<Category> categories = new ArrayList<>();
 }
