2025c67db5d878e8cc78a98b9213395d04b90651
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-04 14:11:52 -0300

    feat: category model

---
+++ src/main/java/com/vinibortoletto/simpleshop/models/Category.java
@@ -0,0 +1,30 @@
+package com.vinibortoletto.simpleshop.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
+    private List<Product> products;
+}
