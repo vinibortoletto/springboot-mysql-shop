20e8a57612f5b07491f5c0240ec5f8189208522e
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-08 18:28:48 -0300

    refactor: model admin and customer

--- src/main/java/com/vinibortoletto/simpleshop/models/Admin.java
+++ src/main/java/com/vinibortoletto/simpleshop/models/Admin.java
@@ -1,14 +1,7 @@
 package com.vinibortoletto.simpleshop.models;
 
-import jakarta.persistence.*;
 
-@Entity
-@Table(name = "tb_admin")
-public class Admin {
-    @Id
-    @OneToOne
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @JoinColumn(name = "id")
-    private String id;
+public class Admin extends User {
+
 }
 
--- src/main/java/com/vinibortoletto/simpleshop/models/Customer.java
+++ src/main/java/com/vinibortoletto/simpleshop/models/Customer.java
@@ -1,33 +1,6 @@
 package com.vinibortoletto.simpleshop.models;
 
-import jakarta.persistence.*;
-import lombok.*;
+public class Customer extends User {
 
-import java.util.HashSet;
-import java.util.Set;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode(callSuper = false)
-@Entity
-@Table(name = "tb_customer")
-public class Customer {
-    @Id
-    @OneToOne
-    @JoinColumn(name = "id")
-    private User user;
-
-    @ManyToMany
-    @JoinTable(
-            name = "tb_customer_address",
-            joinColumns = @JoinColumn(name = "customer_id"),
-            inverseJoinColumns = @JoinColumn(name = "address_id")
-    )
-    private Set<Address> addresses = new HashSet<>();
-
-    @OneToOne(mappedBy = "customer")
-    private Cart cart;
 }
 
