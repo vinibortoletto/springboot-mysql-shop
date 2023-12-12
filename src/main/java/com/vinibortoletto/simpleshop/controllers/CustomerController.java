19908dbae78da701f6e31e72b98a8b6ac17ff03c
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-12 11:10:49 -0300

    refactor: remove unused methods

--- src/main/java/com/vinibortoletto/simpleshop/controllers/CustomerController.java
+++ src/main/java/com/vinibortoletto/simpleshop/controllers/CustomerController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,42 +19,23 @@ import java.util.List;
 @RequestMapping(value = "/customers")
 public class CustomerController {
     @Autowired
-    private CustomerService customerService;
+    private CustomerService service;
 
-    @Operation(summary = "Returns all users")
+    @Operation(summary = "Returns all customers")
     @GetMapping
     public ResponseEntity<List<CustomerResponseDTO>> findAll() {
-        List<Customer> userList = customerService.findAll();
-        List<CustomerResponseDTO> response = CustomerResponseDTO.convert(userList);
+        List<Customer> customerList = service.findAll();
+        List<CustomerResponseDTO> response = CustomerResponseDTO.convert(customerList);
 
         return ResponseEntity.ok().body(response);
     }
 
-    //    @Operation(summary = "Returns a user based on its id")
-//    @GetMapping(value = "/{id}")
-//    public ResponseEntity<User> findById(@PathVariable String id) {
-//        User user = service.findById(id);
-//        return ResponseEntity.ok().body(user);
-//    }
-//
-//    @Operation(summary = "Creates a new user")
-//    @PostMapping
-//    public ResponseEntity<CustomerResponseDTO> save(@RequestBody @Valid CustomerRequestDTO dto) {
-//        Customer customer = service.save(dto);
-//        CustomerResponseDTO response = new CustomerResponseDTO(customer);
-//        return ResponseEntity.status(HttpStatus.CREATED).body(response);
-//    }
-//
-//    @Operation(summary = "Updates a user based on its id")
-//    @PutMapping(value = "/{id}")
-//    public ResponseEntity<User> update(@RequestBody @Valid UserDto dto, @PathVariable String id) {
-//        return ResponseEntity.ok().body(service.update(dto, id));
-//    }
-//
-//    @Operation(summary = "Deletes a user based on its id")
-//    @DeleteMapping(value = "/{id}")
-//    public ResponseEntity<Void> delete(@PathVariable String id) {
-//        service.delete(id);
-//        return ResponseEntity.noContent().build();
-//    }
+    @Operation(summary = "Returns a customer based on its id")
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CustomerResponseDTO> findById(@PathVariable String id) {
+        Customer customer = service.findById(id);
+        CustomerResponseDTO response = new CustomerResponseDTO(customer);
+
+        return ResponseEntity.ok().body(response);
+    }
 }
--- src/main/java/com/vinibortoletto/simpleshop/services/CustomerService.java
+++ src/main/java/com/vinibortoletto/simpleshop/services/CustomerService.java
@@ -1,16 +1,12 @@
 package com.vinibortoletto.simpleshop.services;
 
-import com.vinibortoletto.simpleshop.dtos.CustomerRequestDTO;
-import com.vinibortoletto.simpleshop.exceptions.DatabaseException;
 import com.vinibortoletto.simpleshop.exceptions.NotFoundException;
 import com.vinibortoletto.simpleshop.models.Address;
 import com.vinibortoletto.simpleshop.models.Cart;
 import com.vinibortoletto.simpleshop.models.Customer;
 import com.vinibortoletto.simpleshop.models.User;
 import com.vinibortoletto.simpleshop.repositories.CustomerRepository;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -33,26 +29,6 @@ public class CustomerService {
         return user.orElseThrow(() -> new NotFoundException("Customer not found"));
     }
 
-//    public CustomerResponseDTO save(CustomerRequestDTO dto) {
-//        Optional<Customer> customer = customerRepository.findByEmail(dto.email());
-//
-//        if (customer.isPresent()) {
-//            throw new ConflictException("Email already exists in database");
-//        }
-//
-//        Customer newCustomer = new Customer();
-//        BeanUtils.copyProperties(dto, newCustomer);
-//
-//        Cart cart = cartService.save(newCustomer);
-//        Address address = new Address();
-//
-//        newCustomer.setCart(cart);
-//        newCustomer.getAddresses().add(address);
-//
-//        Customer savedCustomer = customerRepository.save(newCustomer);
-//        return new CustomerResponseDTO(savedCustomer);
-//    }
-
     public Customer save(User user) {
         Customer customer = new Customer();
 
@@ -64,22 +40,6 @@ public class CustomerService {
         customer.setCart(cart);
 
         return customerRepository.save(customer);
-    }
-
-    public Customer update(CustomerRequestDTO dto, String id) {
-        Customer user = findById(id);
-        BeanUtils.copyProperties(dto, user);
-        return customerRepository.save(user);
-    }
-
-    public void delete(String id) {
-        findById(id);
-
-        try {
-            customerRepository.deleteById(id);
-        } catch (DataIntegrityViolationException e) {
-            throw new DatabaseException(e.getMessage());
-        }
     }
 
     public void saveCustomerAddress(Address address, String userId) {
