cf70cf9a4c4682f7b4b62e456df747c3e2a0e6b2
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-12 15:07:10 -0300

    feat: find addresses

--- src/main/java/com/vinibortoletto/simpleshop/controllers/AddressController.java
+++ src/main/java/com/vinibortoletto/simpleshop/controllers/AddressController.java
@@ -10,10 +10,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "addresses")
 @RestController
@@ -22,12 +21,40 @@ public class AddressController {
     @Autowired
     private AddressService addressService;
 
+    @Operation(summary = "Finds all addresses")
+    @GetMapping
+    public ResponseEntity<List<AddressResponseDTO>> findAll() {
+        List<Address> addressList = addressService.findAll();
+        List<AddressResponseDTO> response = AddressResponseDTO.convert(addressList);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @Operation(summary = "Returns an address based on its id")
+    @GetMapping(value = "/{addressId}")
+    public ResponseEntity<AddressResponseDTO> findById(@PathVariable String addressId) {
+        Address address = addressService.findById(addressId);
+        AddressResponseDTO response = new AddressResponseDTO(address);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
     @Operation(summary = "Creates a new address for user")
     @PostMapping
     public ResponseEntity<AddressResponseDTO> save(@RequestBody @Valid AddressRequestDTO dto) {
         Address address = addressService.save(dto);
-        AddressResponseDTO response = new AddressResponseDTO(address, dto.customerId());
+        AddressResponseDTO response = new AddressResponseDTO(address);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+
+    @Operation(summary = "Updates an user address")
+    @PutMapping(name = "/{addressId}")
+    public ResponseEntity<AddressResponseDTO> update(@RequestBody @Valid AddressRequestDTO dto, String addressId) {
+        Address address = addressService.update(dto, addressId);
+        AddressResponseDTO response = new AddressResponseDTO(address);
+
+        return ResponseEntity.ok().body(response);
     }
 }
--- src/main/java/com/vinibortoletto/simpleshop/services/AddressService.java
+++ src/main/java/com/vinibortoletto/simpleshop/services/AddressService.java
@@ -2,6 +2,7 @@ package com.vinibortoletto.simpleshop.services;
 
 import com.vinibortoletto.simpleshop.dtos.AddressRequestDTO;
 import com.vinibortoletto.simpleshop.exceptions.ConflictException;
+import com.vinibortoletto.simpleshop.exceptions.NotFoundException;
 import com.vinibortoletto.simpleshop.models.Address;
 import com.vinibortoletto.simpleshop.models.Customer;
 import com.vinibortoletto.simpleshop.repositories.AddressRepository;
@@ -9,6 +10,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,27 +22,51 @@ public class AddressService {
     @Autowired
     private CustomerService customerService;
 
+    public List<Address> findAll() {
+        return addressRepository.findAll();
+    }
+
+    public Address findById(String id) {
+        Optional<Address> address = addressRepository.findById(id);
+
+        if (address.isEmpty()) {
+            throw new NotFoundException("Address not found");
+        }
+
+        return address.get();
+    }
+
     public Address save(AddressRequestDTO dto) {
         Optional<Address> address = addressRepository.findByZipcodeAndNumber(dto.zipcode(), dto.number());
+        Customer customer = customerService.findById(dto.customerId());
 
         if (address.isPresent()) {
-            Customer customer = customerService.findById(dto.customerId());
             boolean customerHasAddress = customer.getAddresses().contains(address.get());
 
             if (customerHasAddress) {
                 throw new ConflictException("Address already registered by customer");
             }
 
-            customerService.saveCustomerAddress(address.get(), dto.customerId());
+            customerService.saveCustomerAddress(address.get(), customer);
             return address.get();
         }
 
         Address newAddress = new Address();
         BeanUtils.copyProperties(dto, newAddress);
 
-        addressRepository.save(newAddress);
-        customerService.saveCustomerAddress(newAddress, dto.customerId());
+        Address savedAddress = addressRepository.save(newAddress);
+        customerService.saveCustomerAddress(savedAddress, customer);
 
-        return newAddress;
+        return savedAddress;
+    }
+
+
+    public Address update(AddressRequestDTO dto, String id) {
+        findById(id);
+
+        Address newAddress = new Address();
+        BeanUtils.copyProperties(dto, newAddress);
+
+        return addressRepository.save(newAddress);
     }
 }
