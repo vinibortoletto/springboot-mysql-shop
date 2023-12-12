abf27045ca2d369a8001963381b99c7079532d4e
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-12 11:42:32 -0300

    feat: update customer dto

--- src/main/java/com/vinibortoletto/simpleshop/dtos/CustomerResponseDTO.java
+++ src/main/java/com/vinibortoletto/simpleshop/dtos/CustomerResponseDTO.java
@@ -1,13 +1,20 @@
 package com.vinibortoletto.simpleshop.dtos;
 
+import com.vinibortoletto.simpleshop.models.Address;
 import com.vinibortoletto.simpleshop.models.Cart;
 import com.vinibortoletto.simpleshop.models.Customer;
 
 import java.util.List;
 
-public record CustomerResponseDTO(String id, String name, String email, Cart cart) {
+public record CustomerResponseDTO(String id, String name, String email, Cart cart, List<Address> addresses) {
     public CustomerResponseDTO(Customer customer) {
-        this(customer.getId(), customer.getName(), customer.getEmail(), customer.getCart());
+        this(
+                customer.getId(),
+                customer.getName(),
+                customer.getEmail(),
+                customer.getCart(),
+                customer.getAddresses()
+        );
     }
 
     public static List<CustomerResponseDTO> convert(List<Customer> customerList) {
