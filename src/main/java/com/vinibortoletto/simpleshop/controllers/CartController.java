14eb2f4629470249e03b39c51d8b3396f13e6ff3
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-12 17:56:04 -0300

    feat: cart controller

---
+++ src/main/java/com/vinibortoletto/simpleshop/controllers/CartController.java
@@ -0,0 +1,34 @@
+package com.vinibortoletto.simpleshop.controllers;
+
+import com.vinibortoletto.simpleshop.dtos.CartRequestDTO;
+import com.vinibortoletto.simpleshop.dtos.CartResponseDTO;
+import com.vinibortoletto.simpleshop.models.Cart;
+import com.vinibortoletto.simpleshop.services.CartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "carts")
+@RestController
+@RequestMapping(value = "/carts")
+public class CartController {
+    @Autowired
+    private CartService cartService;
+
+    @Operation(summary = "Add product to cart")
+    @PostMapping(value = "/products")
+    public ResponseEntity<CartResponseDTO> addProductToCart(
+            @RequestBody @Valid CartRequestDTO dto
+    ) {
+        Cart cart = cartService.addProductToCart(dto);
+        CartResponseDTO response = new CartResponseDTO(cart);
+
+        return ResponseEntity.ok(response);
+    }
+}
