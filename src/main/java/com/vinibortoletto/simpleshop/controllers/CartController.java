0da2fc085c47a1f8399a92692616efa4731c9d0c
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-13 19:38:52 -0300

    fix: response status

--- src/main/java/com/vinibortoletto/simpleshop/controllers/CartController.java
+++ src/main/java/com/vinibortoletto/simpleshop/controllers/CartController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,6 +30,6 @@ public class CartController {
         Cart cart = cartService.addProductToCart(dto);
         CartResponseDTO response = new CartResponseDTO(cart);
 
-        return ResponseEntity.ok(response);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
