25b8b6ae7c757bd9b4acd2450c5833a80bb310b7
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-14 12:22:40 -0300

    feat: cart total in response

--- src/main/java/com/vinibortoletto/simpleshop/dtos/CartResponseDTO.java
+++ src/main/java/com/vinibortoletto/simpleshop/dtos/CartResponseDTO.java
@@ -2,17 +2,20 @@ package com.vinibortoletto.simpleshop.dtos;
 
 import com.vinibortoletto.simpleshop.models.Cart;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Set;
 
 public record CartResponseDTO(
         String id,
+        BigDecimal total,
         String customerId,
         List<CartProductResponseDTO> products
 ) {
     public CartResponseDTO(Cart cart) {
         this(
                 cart.getId(),
+                cart.getTotal(),
                 cart.getCustomer().getId(),
                 CartProductResponseDTO.convert(cart.getProducts())
         );
