886d648ef3c77db2434b6aa75faa6c6853e09d9b
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-15 17:43:48 -0300

    feat: order product response dto

---
+++ src/main/java/com/vinibortoletto/simpleshop/dtos/OrderProductResponseDTO.java
@@ -0,0 +1,26 @@
+package com.vinibortoletto.simpleshop.dtos;
+
+import com.vinibortoletto.simpleshop.models.OrderProduct;
+import com.vinibortoletto.simpleshop.models.Product;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record OrderProductResponseDTO(
+        Integer quantity,
+        BigDecimal subtotal,
+        Product product
+) {
+
+    public OrderProductResponseDTO(OrderProduct orderProduct) {
+        this(
+                orderProduct.getQuantity(),
+                orderProduct.getSubtotal(),
+                orderProduct.getProduct()
+        );
+    }
+
+    public static List<OrderProductResponseDTO> convert(List<OrderProduct> orderProductList) {
+        return orderProductList.stream().map(OrderProductResponseDTO::new).toList();
+    }
+}
