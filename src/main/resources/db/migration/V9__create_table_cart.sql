a6ceff3fc2fbe87875d8ad17659d6af23474098f
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-15 17:40:23 -0300

    chore: update migration scripts

--- src/main/resources/db/migration/V9__create_table_cart.sql
+++ src/main/resources/db/migration/V9__create_table_cart.sql
@@ -6,4 +6,6 @@ CREATE TABLE tb_cart (
 );
 
 INSERT INTO tb_cart (id, total, customer_id)
-VALUES ("50e54531-2ee8-4554-ae76-231cceac3311", 129.95, "5fef16b9-fc9b-4878-b09a-48efe4652933");
+VALUES
+    ("cdd4ffcf-ef8e-4a24-9e3d-e333e159b7f2", 129.95, "5fef16b9-fc9b-4878-b09a-48efe4652933"),
+    ("50e54531-2ee8-4554-ae76-231cceac3311", 129.95, "ca5cc4c9-0e04-4b8c-a3be-6e9f2e050148");
