a7ee277f8854efc4031ea9585db1f7b1f441fbb9
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-15 17:43:19 -0300

    fix: enum type

--- src/main/java/com/vinibortoletto/simpleshop/models/User.java
+++ src/main/java/com/vinibortoletto/simpleshop/models/User.java
@@ -24,6 +24,8 @@ public class User implements Serializable {
     private String name;
     private String email;
     private String password;
+
+    @Enumerated(EnumType.STRING)
     private Role role;
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
