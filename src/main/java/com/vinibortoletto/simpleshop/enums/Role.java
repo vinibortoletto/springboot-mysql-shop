1d2d2dcea64d2c00d3bbfa9e84b16d1ce15ccc78
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-19 18:48:03 -0300

    refactor: role enum

--- src/main/java/com/vinibortoletto/simpleshop/enums/Role.java
+++ src/main/java/com/vinibortoletto/simpleshop/enums/Role.java
@@ -1,6 +1,13 @@
 package com.vinibortoletto.simpleshop.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Role {
-    ADMIN,
-    CUSTOMER
+    ADMIN("ADMIN"),
+    CUSTOMER("CUSTOMER");
+
+    private final String role;
 }
