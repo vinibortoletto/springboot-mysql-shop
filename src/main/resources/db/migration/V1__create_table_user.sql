43287a8d83876a0d49478e3c6808c458e7486cc0
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-27 18:04:02 -0300

    chore: update migration scripts

--- src/main/resources/db/migration/V1__create_table_user.sql
+++ src/main/resources/db/migration/V1__create_table_user.sql
@@ -8,6 +8,6 @@ CREATE TABLE tb_user (
 
 INSERT INTO tb_user (id, name, email, password, role)
 VALUES
-    ('1c1ffd91-d9f9-4327-9c94-bd3e84644cd6', 'Admin User', 'admin@example.com', 'admin_password', 'ADMIN'),
-    ('ca5cc4c9-0e04-4b8c-a3be-6e9f2e050148', 'Customer 1 User', 'customer1@example.com', 'customer_password', 'CUSTOMER'),
-    ('5fef16b9-fc9b-4878-b09a-48efe4652933', 'Customer 2 User', 'customer2@example.com', 'customer_password', 'CUSTOMER');
+    ('1c1ffd91-d9f9-4327-9c94-bd3e84644cd6', 'Admin User', 'admin@example.com', '$2a$10$63xUNq..HSIsC5/GFxYCD.MpJ1yGM2peVUR4YiZX.EEL3z1GOh5.u', 'ADMIN'),
+    ('ca5cc4c9-0e04-4b8c-a3be-6e9f2e050148', 'Customer 1 User', 'customer1@example.com', '$2a$10$ZT56grLwe0oVqeOleWyMXO9c5uALwQgoV4CjQeNm8k7xNTkRO4ysy', 'CUSTOMER'),
+    ('5fef16b9-fc9b-4878-b09a-48efe4652933', 'Customer 2 User', 'customer2@example.com', '$2a$10$uNVkRCICeeLOQNaIeemCNeZ05cFxf4sMubVn5A//OGKpHVtfWhhy2', 'CUSTOMER');
