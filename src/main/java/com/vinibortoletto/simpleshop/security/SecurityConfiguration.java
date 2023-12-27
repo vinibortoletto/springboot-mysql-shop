5b03ecaeeea538a90e09bcd3713b899317a8c660
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-27 17:20:07 -0300

    feat: security and jwt config

--- src/main/java/com/vinibortoletto/simpleshop/security/SecurityConfiguration.java
+++ src/main/java/com/vinibortoletto/simpleshop/security/SecurityConfiguration.java
@@ -1,26 +1,68 @@
 package com.vinibortoletto.simpleshop.security;
 
+import com.vinibortoletto.simpleshop.enums.Role;
+import com.vinibortoletto.simpleshop.repositories.UserRepository;
+import com.vinibortoletto.simpleshop.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(new AntPathRequestMatcher("/products", "POST")).hasRole("ADMIN")
-                        .anyRequest().authenticated()
-                )
-                .build();
-    }
+  @Autowired
+  private UserRepository userRepository;
+
+  @Autowired
+  private UserService userService;
+
+  @Autowired
+  private SecurityFilter securityFilter;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity
+      .csrf(AbstractHttpConfigurer::disable)
+      .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+      .authorizeHttpRequests(authorize -> authorize
+        .requestMatchers(new AntPathRequestMatcher("/users/login", "POST")).permitAll()
+        .requestMatchers(new AntPathRequestMatcher("/users", "POST")).permitAll()
+        .requestMatchers(new AntPathRequestMatcher("/products", "POST")).hasRole(Role.ADMIN.getRole())
+        .anyRequest().authenticated()
+      )
+      .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+      .build();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(email -> userService.findByEmail(email));
+    authProvider.setPasswordEncoder(passwordEncoder());
+
+    return authProvider;
+  }
 }
