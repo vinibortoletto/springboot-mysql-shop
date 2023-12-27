ba035c8f11c02d097d62c5f6e80181c0a72b0241
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-27 17:20:33 -0300

    feat: login endpoint

--- src/main/java/com/vinibortoletto/simpleshop/controllers/UserController.java
+++ src/main/java/com/vinibortoletto/simpleshop/controllers/UserController.java
@@ -1,8 +1,11 @@
 package com.vinibortoletto.simpleshop.controllers;
 
+import com.vinibortoletto.simpleshop.dtos.user.AuthenticationRequestDTO;
+import com.vinibortoletto.simpleshop.dtos.user.AuthenticationResponseDTO;
 import com.vinibortoletto.simpleshop.dtos.user.UserRequestDTO;
 import com.vinibortoletto.simpleshop.dtos.user.UserResponseDTO;
 import com.vinibortoletto.simpleshop.models.User;
+import com.vinibortoletto.simpleshop.security.TokenService;
 import com.vinibortoletto.simpleshop.services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,6 +13,8 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,49 +23,66 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
-    @Autowired
-    private UserService userService;
+  @Autowired
+  private AuthenticationManager authenticationManager;
 
-    @Operation(summary = "Returns all users")
-    @GetMapping
-    public ResponseEntity<List<UserResponseDTO>> findAll() {
-        List<User> userList = userService.findAll();
-        List<UserResponseDTO> response = UserResponseDTO.convert(userList);
+  @Autowired
+  private UserService userService;
 
-        return ResponseEntity.ok().body(response);
-    }
+  @Autowired
+  private TokenService tokenService;
 
-    @Operation(summary = "Returns a user based on its id")
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<UserResponseDTO> findById(@PathVariable String id) {
-        User user = userService.findById(id);
-        UserResponseDTO response = new UserResponseDTO(user);
+  @Operation(summary = "Returns all users")
+  @GetMapping
+  public ResponseEntity<List<UserResponseDTO>> findAll() {
+    List<User> userList = userService.findAll();
+    List<UserResponseDTO> response = UserResponseDTO.convert(userList);
 
-        return ResponseEntity.ok().body(response);
-    }
+    return ResponseEntity.ok().body(response);
+  }
 
-    @Operation(summary = "Creates a new user")
-    @PostMapping
-    public ResponseEntity<UserResponseDTO> save(@RequestBody @Valid UserRequestDTO dto) {
-        User user = userService.save(dto);
-        UserResponseDTO response = new UserResponseDTO(user);
+  @Operation(summary = "Returns a user based on its id")
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<UserResponseDTO> findById(@PathVariable String id) {
+    User user = userService.findById(id);
+    UserResponseDTO response = new UserResponseDTO(user);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
-    }
+    return ResponseEntity.ok().body(response);
+  }
 
-    @Operation(summary = "Updates a user based on its id")
-    @PutMapping(value = "/{id}")
-    public ResponseEntity<UserResponseDTO> update(@RequestBody @Valid UserRequestDTO dto, @PathVariable String id) {
-        User user = userService.update(dto, id);
-        UserResponseDTO response = new UserResponseDTO(user);
+  @Operation(summary = "Creates a new user")
+  @PostMapping
+  public ResponseEntity<UserResponseDTO> save(@RequestBody @Valid UserRequestDTO dto) {
+    User user = userService.save(dto);
+    UserResponseDTO response = new UserResponseDTO(user);
 
-        return ResponseEntity.ok().body(response);
-    }
+    return ResponseEntity.status(HttpStatus.CREATED).body(response);
+  }
 
-    @Operation(summary = "Deletes a user based on its id")
-    @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Void> delete(@PathVariable String id) {
-        userService.delete(id);
-        return ResponseEntity.noContent().build();
-    }
+  @Operation(summary = "Updates a user based on its id")
+  @PutMapping(value = "/{id}")
+  public ResponseEntity<UserResponseDTO> update(@RequestBody @Valid UserRequestDTO dto, @PathVariable String id) {
+    User user = userService.update(dto, id);
+    UserResponseDTO response = new UserResponseDTO(user);
+
+    return ResponseEntity.ok().body(response);
+  }
+
+  @Operation(summary = "Deletes a user based on its id")
+  @DeleteMapping(value = "/{id}")
+  public ResponseEntity<Void> delete(@PathVariable String id) {
+    userService.delete(id);
+    return ResponseEntity.noContent().build();
+  }
+
+  @Operation(summary = "Validate user login")
+  @PostMapping("/login")
+  public ResponseEntity<AuthenticationResponseDTO> login(@RequestBody @Valid AuthenticationRequestDTO dto) {
+    var usernamePassword = new UsernamePasswordAuthenticationToken(dto.email(), dto.password());
+    var auth = authenticationManager.authenticate(usernamePassword);
+    String token = tokenService.generateToken((User) auth.getPrincipal());
+    AuthenticationResponseDTO response = new AuthenticationResponseDTO(token);
+
+    return ResponseEntity.ok().body(response);
+  }
 }
