e54eaf70862f66ce642c7fdc036b4251a788bdf4
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-12-29 16:21:18 -0300

    feat: white list matchers

--- src/main/java/com/vinibortoletto/simpleshop/security/SecurityConfiguration.java
+++ src/main/java/com/vinibortoletto/simpleshop/security/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import com.vinibortoletto.simpleshop.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -19,50 +20,57 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-  @Autowired
-  private UserRepository userRepository;
+    private static final RequestMatcher[] WHITE_LIST = {
+        new AntPathRequestMatcher("/swagger-ui/**"),
+        new AntPathRequestMatcher("/swagger-ui.html"),
+        new AntPathRequestMatcher("/v2/api-docs"),
+        new AntPathRequestMatcher("/v3/api-docs/**"),
+        new AntPathRequestMatcher("/users/login", HttpMethod.POST.name()),
+        new AntPathRequestMatcher("/users", HttpMethod.POST.name())
+    };
 
-  @Autowired
-  private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private SecurityFilter securityFilter;
 
-  @Autowired
-  private SecurityFilter securityFilter;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(WHITE_LIST).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/users", HttpMethod.GET.name())).hasRole(Role.ADMIN.getRole())
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
+    }
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-    return httpSecurity
-      .csrf(AbstractHttpConfigurer::disable)
-      .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-      .authorizeHttpRequests(authorize -> authorize
-        .requestMatchers(new AntPathRequestMatcher("/users/login", "POST")).permitAll()
-        .requestMatchers(new AntPathRequestMatcher("/users", "POST")).permitAll()
-        .requestMatchers(new AntPathRequestMatcher("/products", "POST")).hasRole(Role.ADMIN.getRole())
-        .anyRequest().authenticated()
-      )
-      .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-      .build();
-  }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
-  @Bean
-  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-    return authenticationConfiguration.getAuthenticationManager();
-  }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(email -> userService.findByEmail(email));
+        authProvider.setPasswordEncoder(passwordEncoder());
 
-  @Bean
-  public AuthenticationProvider authenticationProvider() {
-    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-    authProvider.setUserDetailsService(email -> userService.findByEmail(email));
-    authProvider.setPasswordEncoder(passwordEncoder());
-
-    return authProvider;
-  }
+        return authProvider;
+    }
 }
