7afbc0f74ae1d622e2d7122501fc812d0a09061b
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2024-01-11 12:07:19 -0300

    fix: cart product query

--- src/main/java/com/vinibortoletto/simpleshop/repositories/CartProductRepository.java
+++ src/main/java/com/vinibortoletto/simpleshop/repositories/CartProductRepository.java
@@ -2,11 +2,14 @@ package com.vinibortoletto.simpleshop.repositories;
 
 import com.vinibortoletto.simpleshop.models.CartProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface CartProductRepository extends JpaRepository<CartProduct, String> {
-    Optional<CartProduct> findByCartIdAndProductId(String cartId, String productId);
+    @Query(value = "SELECT cp FROM cart_product WHERE cp.cart_id = :cartId AND cp.product_id = :productId", nativeQuery = true)
+    Optional<CartProduct> findByCartIdAndProductId(@Param("cartId") String cartId, @Param("productId") String productId);
 }
